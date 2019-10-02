98e3e0e996df822ea825e3f598f69954c59a6c56
Mike Boyle <mikesboyle@gmail.com>
2019-10-02 01:22:49 -0400

    puppeteer tests added

--- integration/app.test.js
+++ integration/app.test.js
@@ -12,4 +12,16 @@ describe("Word Counter", () => {
    * https://github.com/smooth-code/jest-puppeteer
    * https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#class-page
    */
+  it("should count the words in a text", async () => {
+    await page.type('[name="wordCounterInput"]', "Goodbye World!");
+    await page.click('[data-testid="wordCounterSubmit"]');
+    await expect(page).toMatch("Total length: 2 words");
+  });
+
+  it("displays an error when there is no text", async () => {
+    await page.click('[data-testid="wordCounterSubmit"]');
+    await expect(page).toMatch(
+      "Your text seems to be blank. Please type something and try again."
+    );
+  });
 });
